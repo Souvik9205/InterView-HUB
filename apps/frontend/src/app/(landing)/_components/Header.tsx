063a971d9d103fa0beaa1ad1371753dc1f469ada
Sayan Das <sayandas61668@gmail.com>
2025-02-24 21:50:44 +0530

    2nd commite

--- apps/frontend/src/app/(landing)/_components/Header.tsx
+++ apps/frontend/src/app/(landing)/_components/Header.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const Header = () => {
   return (
-    <nav className="fixed backdrop-blur-md top-7 left-0 right-0 z-50 flex items-center justify-center">
-      <div className="w-[1000px] h-[85px] rounded-full bg-[#0c0c0cd9] p-3 flex items-center justify-between">
+    <nav className="fixed top-7 left-0 right-0 z-50 flex items-center justify-center">
+      <div className="w-[1000px] backdrop-blur-md h-[85px] rounded-full bg-[#0c0c0cd9] p-3 flex items-center justify-between">
         <div className="bg-[#f8f8f8] w-[130px] rounded-full h-full flex items-center justify-center font-semibold text-2xl">Sayan</div>
         <div className="flex items-center justify-between gap-10 text-white rounded-full h-full">
           {[
